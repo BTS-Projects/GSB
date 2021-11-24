ecd8c9518028726173985168bb17d0c287f72547
Lempereurjulien <lempereur.julien83@gmail.com>
2021-11-24 16:49:49 +0100

    ajout de name RficheFrais et AficheFrais dans v-accueil

--- js/j_validerFrais.js
+++ js/j_validerFrais.js
@@ -1,4 +1,8 @@
 function clickSelectionnerMois(){
+    
+    var tabMois = "";
+    var nb_mois = 0;
+    var dep_texte = document.getElementByID(1stemp).value;
     window.alert("salut");
     //var result ='<?php href="index.php?uc=validerFrais&action=selectionnerMois ?>"' 
 }
