035ad6339b1f0c51e0a4d1a276f6353c0f2766e1
Lempereurjulien <lempereur.julien83@gmail.com>
2021-12-01 17:00:50 +0100

    Ajout de onchange lien

--- vues/vuesComptables/v_valideFrais.php
+++ vues/vuesComptables/v_valideFrais.php
@@ -8,7 +8,7 @@
 <form role="form" method="post">
     <script language='javascript' id="cible" src="js/j_validerFrais.js"></script>
     <label for="lstemp" accesskey="l">choisir le visiteur :</label>
-    <select id="lstemp" name="lstemp" class="form-control" style="width: 200px" onchange="index.php?uc = validerFrais & action = selectionnerMois">
+    <select id="lstemp" name="lstemp" class="form-control" style="width: 200px" onchange=window.location.href="index.php?uc=validerFrais&action=selectionnerMois">
         <?php
         //on recuperer les visiteurs pour les pouvoir les afficher
         foreach ($lesNomsvisiteurs as $visiteurs) {
